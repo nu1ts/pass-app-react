107585abfb96e90564e101ab24bae3ed85e938ec
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-02-26 23:00:36 +0700

    styled dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { Archive, NotificationsActive } from '@mui/icons-material';
+import { Archive, Home, NotificationsActive } from '@mui/icons-material';
 import { ReactRouterAppProvider } from '@toolpad/core/react-router';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import { Outlet } from 'react-router-dom';
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import BadgeIcon from '@mui/icons-material/Badge';
+import PeopleIcon from '@mui/icons-material/People';
+import HistoryIcon from '@mui/icons-material/History';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import { Outlet } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 import './scss/styles.scss';
 import './scss/stylesMUI.scss';
 import appTheme from './themes/appTheme';
 
 const navigation = [
     {
-        segment: '',
-        title: 'Главная',
-        icon: <DashboardIcon />,
+        kind: 'header',
+        title: 'Управление',
     },
     {
-        segment: 'notifications',
-        title: 'Уведомления',
-        icon: <NotificationsActive />,
+        segment: '',
+        title: 'Главная',
+        icon: <HomeIcon />,
     },
     {
         segment: 'archive',
-        title: 'Архив пропусков',
-        icon: <Archive />,
+        title: 'Заявки',
+        icon: <NotificationsActive />,
     },
     {
-        segment: 'reports',
-        title: 'Отчет',
-        icon: <BarChartIcon />,
+        segment: 'notifications',
+        title: 'Пользователи',
+        icon: <PeopleIcon />,
+    },
+
+    {
+        segment: 'absence',
+        title: 'История пропусков',
+        icon: <HistoryIcon />,
+    },
+    {
+        kind: 'divider',
+    },
+    {
+        kind: 'header',
+        title: 'Действия',
+    },
+    {
+        segment: '/logout',
+        title: 'Выйти',
+        icon: <ExitToAppIcon />,
     },
 ];
 
