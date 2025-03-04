8d164d607a70b27d594c45e94f608a988f766b14
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-03-04 11:55:34 +0700

    modified InfoChip style properties

--- src/components/chip/InfoChip.jsx
+++ src/components/chip/InfoChip.jsx
@@ -7,15 +7,20 @@ const InfoChip = ({ title = '', color = 'info' }) => {
             <Chip
                 sx={{
                     borderRadius: '8px',
+                    border: '1px solid',
                     boxSizing: 'border-box',
-                    minWidth: '120px',
-                    padding: '6px 0',
+                    minWidth: '90px',
+                    padding: '6px 4px ',
                     width: 'auto',
                     height: 'auto',
+                    fontWeight: '600',
+                    fontSize: '10px',
+                    textTransform: 'uppercase',
                 }}
                 size='small'
                 label={title}
                 color={color}
+                variant={'outlined'}
             />
         </>
     );
