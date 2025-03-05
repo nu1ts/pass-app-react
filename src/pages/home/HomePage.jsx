1a038a9283bd5f38c88287c183f0b804804d92b8
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-03-06 00:43:13 +0700

    modified home scss properties

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,9 +5,17 @@ const HomePage = () => {
     return (
         <div>
             <section className='content'>
-                <h2>
-                    Добро пожаловать в систему управления и администрирования пропусками TSU.PASS
-                </h2>
+                <div className='background'>
+                    <div className='greeting'>
+                        <h2>
+                            Добро пожаловать в систему управления и администрирования пропусками
+                            TSU.PASS
+                        </h2>
+                        <div className='round top-left'></div>
+                        <div className='round bottom-right l-size'></div>
+                    </div>
+                    <div className='round'></div>
+                </div>
             </section>
         </div>
     );
